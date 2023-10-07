1e41201083de65ce57dbc4718b86f04e7c464f88
backdevhong <ghddls0704@kakao.com>
2023-10-08 03:00:58 +0900

    plz

--- commands/game/game.js
+++ commands/game/game.js
@@ -230,7 +230,7 @@ module.exports = {
             });
 
             collector.on("end", async (i) => {
-              const channel = response
+              const channel = response.member.guild
 
               // const userList = await prisma.currentGameUsers.findMany({
               //   where: {
